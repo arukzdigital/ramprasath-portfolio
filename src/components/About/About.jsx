4345ba51f6d2260fd28a3fec7179282ae7293b15
muthukuttiamk <muthukuttiamk@gmail.com>
2026-02-22 13:16:44 +0530

    Change About profile photo and add interactive hover scale/rotate animations

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import profileImg from '../../assets/1000268674.jpg.jpeg';
+import profileImg from '../../assets/1000289083.jpg.jpeg';
 import './About.css';
 
 const About = () => {
